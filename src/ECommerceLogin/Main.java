7fc6d0d4d48345a2ca56559b5e9075249f6a121c
volkan karaali <volkankaraali1@gmail.com>
2021-05-07 01:18:02 +0300

    first

--- src/ECommerceLogin/Main.java
+++ src/ECommerceLogin/Main.java
@@ -15,14 +15,23 @@ public class Main {
 		
 		User user1=new User(1,"volkan","karaali","volkan@mail.com","123456");
 		User user2=new User(2,"test","test","volkan@mail.com","123456");
+		User user3=new User(3,"test2","test2","test2@mail.com","123456");
+		User user4=new User(4,"test3","test3","test3@mail.com","123456");
 
 		UserManager userManager=new UserManager(new HibernateUserDao(),new EmailValidateManager());
 		
 		userManager.register(user1);
 		System.out.println("\n*****Kayit Hata Mesaji******");
 		userManager.register(user2);
+		System.out.println("\n*****Kayit Mesaji******");
+		userManager.register(user3);
+		System.out.println("\n*****Kayit Mesaji******");
+		userManager.register(user4);
 		
-		
+		System.out.println("\n*****Kullanicilar******");
+		for (var user : userManager.getAll()) {
+			System.out.println(user.getName());
+		}
 		
 		System.out.println("\n*****Login******");
 		userManager.login(user1);
